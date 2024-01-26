df2ab881ec5b43ee26d1c4e3cb6bb38999a957d8
Sashimi-iiii <42736431+Sashimi-iiii@users.noreply.github.com>
2024-01-26 17:57:43 +0100

    Update Program.cs
    2a commit dal server

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,8 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Changed");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
         }
     }
 }
